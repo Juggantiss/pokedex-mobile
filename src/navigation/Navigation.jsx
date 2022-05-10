251a0bb321665155730873096bc53bbd6d3839bf
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-05-10 17:26:38 -0500

    Bottom Menu terminado ya con sus iconos

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,5 +1,6 @@
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import AccountScreen from "../screens/Account";
 import FavoriteScreen from "../screens/Favorite";
 import PokedexScreen from "../screens/Pokedex";
@@ -8,24 +9,45 @@ const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
-    <Tab.Navigator
-      initialRouteName="Account"
-      screenOptions={{
-        tabBarActiveTintColor: "#4FBDBA"
-      }}
-    >
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: "Favoritos",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" size={size} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexScreen}
+        options={{
+          tabBarLabel: "",
+          headerShown: false,
+          tabBarIcon: () => renderPokeball()
+        }}
+      />
       <Tab.Screen
         name="Account"
         component={AccountScreen}
         options={{
+          tabBarLabel: "Mi cuenta",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
+            <FontAwesome name="user-o" size={size} color={color} />
           )
         }}
       />
-      <Tab.Screen name="Pokedex" component={PokedexScreen} />
-      <Tab.Screen name="Favorite" component={FavoriteScreen} />
     </Tab.Navigator>
   );
 }
+
+const renderPokeball = () => (
+  <Image
+    source={require("../assets/pokeball.png")}
+    style={{ width: 75, height: 75, top: -15 }}
+  />
+);
