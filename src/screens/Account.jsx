448009c11a54dc2c1cbad178ae25fe93c9742a50
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-05-11 14:26:22 -0500

    Creado sistema de autenticacion demo

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -1,10 +1,14 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
+import LoginForm from "../components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
 
 export default function Account() {
+  const auth = null;
+
   return (
-    <SafeAreaView>
+    <View>
+      {auth ? <UserData /> : <LoginForm />}
       <Text>Account</Text>
-    </SafeAreaView>
+    </View>
   );
 }
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "juggantiss",
+  password: "123456"
+};
+
+export const userDetails = {
+  username: "juggantiss",
+  firstname: "Juan",
+  lastname: "Martinez",
+  email: "juggantiss@gmail.com"
+};
